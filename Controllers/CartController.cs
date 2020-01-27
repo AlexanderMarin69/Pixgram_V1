4c2e9a82bd2e0e84de153b700fca9b53ca2ba991
Alexander Marin <maal18en@student.ju.se>
2020-01-27 13:42:33 +0100

    All download rewqs works stable

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -52,6 +54,41 @@ namespace Pixgram_V1.Controllers
             }
 
             return RedirectToAction(nameof(Index), new { returnUrl });
+        }
+
+        public IActionResult DownloadSingleImage(int UploadIdForDownload)
+        {
+            var FileUplaodToDownlaod = ctx.FileUploads.Where(x => x.Id == UploadIdForDownload).FirstOrDefault();
+
+            byte[] bytes;
+
+            using (var ms = new MemoryStream())
+            {
+                using (var imagezip = new ZipArchive(ms, ZipArchiveMode.Create, true))
+                        imagezip.CreateEntryFromFile($"C:/Users/alexa/source/repos/Pixgram_V1/wwwroot{FileUplaodToDownlaod.FilePath}", FileUplaodToDownlaod.FileTitle.ToString() + ".jpg", CompressionLevel.Fastest);
+                ms.Position = 0;
+                bytes = ms.ToArray();
+            }
+            return File(bytes, "application/zip", "image.zip");
+        }
+
+        public IActionResult DownloadAllImagesZip()
+        {
+            byte[] bytes;
+            using (var ms = new MemoryStream())
+            {
+                using (var imagezip = new ZipArchive(ms, ZipArchiveMode.Create, true))
+                    foreach (var image in cart.CartRows)
+                        imagezip.CreateEntryFromFile($"C:/Users/alexa/source/repos/Pixgram_V1/wwwroot{image.FileUpload.FilePath}", image.FileUpload.FileTitle.ToString() + ".jpg", CompressionLevel.Fastest);
+                ms.Position = 0;
+                bytes = ms.ToArray();
+            }
+            return File(bytes, "application/zip", "images.zip");
+        }
+
+        public IActionResult DownloadSingleImageZip()
+        {
+            return View();
         }
     }
 }
